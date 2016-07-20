9d83cdba1e1987367e659213aebf2945a183e613
lucas-borges <lucasrborges94@gmail.com>
2016-07-20 14:42:14 -0500

    fixed missing index in list of lists, added output redirection to file

--- server/fleet.py
+++ server/fleet.py
@@ -2,8 +2,8 @@ from drone import drone
 
 
 class Fleet():
-	# [droneObj, port]
-	drone_list = [[None, "com6"],[None, "com7"]]
+	# [droneObj, port, logfile]
+	drone_list = [[None, "com6", None],[None, "com7", None]]
 
 
 	def request(self, lat, lon): #returns id or -1
@@ -14,38 +14,42 @@ class Fleet():
 		"""
 		for i in range(len(drone_list)):
 			if drone_list[i][0] == None:
-				drone_list[i][0] = Drone(drone_list[i][1], lat, lon, altitude=10)
+				fo = open("log/{:%Y-%m-%d %H:%M:%S}::{}".format(datetime.datetime.now(),i))
+				drone_list[i][2] = fo
+				drone_list[i][0] = Drone(drone_list[i][1], lat, lon, altitude=10, output=fo)
 				return i
 
 		return -1
 
 	def connect(self, id):
 		"""Connects to drone with id."""
-		drone_list[id].connect()
+		drone_list[id][0].connect()
 
 	def run(self, id):
 		"""Upload and execute mission on drone with id."""
-		drone_list[id].run()
+		drone_list[id][0].run()
 
 	def get_location(self, id):
 		"""Gets tuple (lat, lon) of drone with id."""
-		return drone_list[id].get_location()
+		return drone_list[id][0].get_location()
 
 	def mission_ended(self, id):
 		"""
 		Returns True if drone with id has completed its mission, False otherwise.
 		disconnect() needs to be called on the drone to free it for a new mission.
 		"""
-		return drone_list[id].mission_ended()
+		return drone_list[id][0].mission_ended()
 
 	def disconnect(self, id):
 		"""Disconnects from drone with id and frees it for use."""
-		drone_list[id].close()
-		drone_list[id] = None
+		drone_list[id][0].close()
+		drone_list[id][0] = None
+		drone_list[id][2].close()
+		drone_list[id][2] = None
 
 	def get_status(self,id):
 		"""
 		Gets status of drone with id.
 		Returns dictionary with fields: current, total, command, distance and altitude.
 		"""
-		return drone_list[id].get_status()
+		return drone_list[id][0].get_status()
