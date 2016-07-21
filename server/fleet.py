c3c92de139912afae0195512f03e76feaac57e9c
lucas-borges <lucasrborges94@gmail.com>
2016-07-21 14:26:03 -0500

    fixed fleet dependancy

--- server/fleet.py
+++ server/fleet.py
@@ -1,55 +1,62 @@
 from drone import drone
+import datetime
 
 
 class Fleet():
-	# [droneObj, port, logfile]
-	drone_list = [[None, "com6", None],[None, "com7", None]]
+    # [droneObj, port, logfile]
+    drone_list = [[None, "com6", None],[None, "com7", None]]
 
+    def __init__(self):
+        pass
 
-	def request(self, lat, lon): #returns id or -1
-		""" 
-		Requests for a drone.
-		Returns the id of the drone if there is one available.
-		Returns -1 if all drones are occupied.
-		"""
-		for i in range(len(drone_list)):
-			if drone_list[i][0] == None:
-				fo = open("log/{:%Y-%m-%d %H:%M:%S}::{}".format(datetime.datetime.now(),i))
-				drone_list[i][2] = fo
-				drone_list[i][0] = Drone(drone_list[i][1], lat, lon, altitude=10, output=fo)
-				return i
+    def request(self, lat, lon): #returns id or -1
+        """ 
+        Requests for a drone.
+        Returns the id of the drone if there is one available.
+        Returns -1 if all drones are occupied.
+        """
+        for i in range(len(self.drone_list)):
+            if self.drone_list[i][0] == None:
+                fo = open("logs/{:%Y-%m-%d..%H.%M.%S}..{}.txt".format(datetime.datetime.now(),i), "w")
+                self.drone_list[i][2] = fo
+                self.drone_list[i][0] = drone.Drone(self.drone_list[i][1], lat, lon, altitude=10, output=fo)
+                return i
 
-		return -1
+        return -1
 
-	def connect(self, id):
-		"""Connects to drone with id."""
-		drone_list[id][0].connect()
+    def connect(self, id):
+        """Connects to drone with id."""
+        self.drone_list[id][0].connect()
+        # print "!!!!!!!!!! connecting to drone :: {}".format(id)
+        # import time
+        # while True:
+        #   time.sleep(1)
 
-	def run(self, id):
-		"""Upload and execute mission on drone with id."""
-		drone_list[id][0].run()
+    def run(self, id):
+        """Upload and execute mission on drone with id."""
+        self.drone_list[id][0].run()
 
-	def get_location(self, id):
-		"""Gets tuple (lat, lon) of drone with id."""
-		return drone_list[id][0].get_location()
+    def get_location(self, id):
+        """Gets tuple (lat, lon) of drone with id."""
+        return self.drone_list[id][0].get_location()
 
-	def mission_ended(self, id):
-		"""
-		Returns True if drone with id has completed its mission, False otherwise.
-		disconnect() needs to be called on the drone to free it for a new mission.
-		"""
-		return drone_list[id][0].mission_ended()
+    def mission_ended(self, id):
+        """
+        Returns True if drone with id has completed its mission, False otherwise.
+        disconnect() needs to be called on the drone to free it for a new mission.
+        """
+        return self.drone_list[id][0].mission_ended()
 
-	def disconnect(self, id):
-		"""Disconnects from drone with id and frees it for use."""
-		drone_list[id][0].close()
-		drone_list[id][0] = None
-		drone_list[id][2].close()
-		drone_list[id][2] = None
+    def disconnect(self, id):
+        """Disconnects from drone with id and frees it for use."""
+        self.drone_list[id][0].close()
+        self.drone_list[id][0] = None
+        self.drone_list[id][2].close()
+        self.drone_list[id][2] = None
 
-	def get_status(self,id):
-		"""
-		Gets status of drone with id.
-		Returns dictionary with fields: current, total, command, distance and altitude.
-		"""
-		return drone_list[id][0].get_status()
+    def get_status(self,id):
+        """
+        Gets status of drone with id.
+        Returns dictionary with fields: current, total, command, distance and altitude.
+        """
+        return self.drone_list[id][0].get_status()
