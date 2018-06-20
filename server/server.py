31728bb989f0272e594009ec2fa2a88a07cbc98a
nayoung kim <nkim21@hawk.iit.edu>
2018-06-20 15:16:52 -0500

    updated code to receive info as a dictoinary

--- server/server.py
+++ server/server.py
@@ -1,28 +1,32 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from io import BytesIO
-import SimpleHTTPServer
-import SocketServer
+#from io import BytesIO
+#import SimpleHTTPServer 
+#import SocketServer
 import logging
 import logging.handlers
 import sqlite3
 import string
-import urlparse
 import json
-import requests
+#import requests
 import re
 import operator
 import sys
-import subprocess
-
-"""
-sqlite3
-"""
-
-
+import subprocess 
+from decimal import Decimal
+#import urllib.request
+import urllib.parse
+#from urllib.parse import urlparse
 
 
 """
-logging
+#sqlite3
+"""
+
+
+
+
+"""
+#logging
 """
 
 
@@ -59,44 +63,25 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 	def do_POST(self):
 		content_length = int(self.headers['Content-Length'])
-		body = self.rfile.read(content_length)
 
-		"""
-		"instanceID":"fb1e7622-8dce-4835-8c8d-fdbad655910c",
-		"requestID":"REQUEST_12-Jun-2018_11:31:31:3131",
-		"latitude":41.823755168694916,
-		"longitude":-87.62914348393679,
-		"address":"108 W Pershing Rd, Chicago, IL 60609, USA"
-		"drone type":1
-		"""
+		data = self.rfile.read(content_length).decode('utf-8')
+		print(data)
+		print(type(data))
+		json_data = json.loads(data)
+		print(data)
+		#print(data.keys())
+		post_data=urllib.parse.parse_qs(data)
+
 
 		self.send_response(200)
-		response = BytesIO()
+		#response = BytesIO()
 
 		print('This is POST request. ')
-		
-		print(body)  #print all post information but not parsed, in dictionary (key,value)
-	
-		
-		#dict = {'instanceID' : 'fb1e7622-8dce-4835-8c8d-fdbad655910c', 'latitude' : '41.823755168694916' , 'longtitude' : '-87.62914348393679', 'address' : '108 W Pershing Rd, Chicago, IL 60609, USA'}
-		#put temporary information
+
+		print("latitude :", json_data['latitude'])  
+		print("longitude :", json_data['longitude']) 
 
 
-		#for key, value in dict.items():
-				#print(key, value)
 
-		# print dict
-		
-		#with open("example.json",'wb') as f:
-		#	json.dump(dict,f)
-		# make json file 
-
-		lon=body.get("longitude","none")
-		lat =body.get("latitude","none")
-		
-		
-
-		subprocess.Popen(['python', '../server/drone/launch.py', '/dev/ttyUSB0', lat, lon, '0'])
-	
 httpd = HTTPServer(('10.0.0.10', 8080), SimpleHTTPRequestHandler)
 httpd.serve_forever() 
